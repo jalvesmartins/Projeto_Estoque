cb534993db4d31fd75d4f786d9657ee387ac505a
jalvesmartins <jalvesmartins16@gmail.com>
2024-05-14 22:41:55 -0300

    Adição função no service - Ver valores totais.

--- service/serviceEstoque.ts
+++ service/serviceEstoque.ts
@@ -43,6 +43,19 @@ class ServiceEstoque {
         fs.appendFileSync(filePath, 'nome,peso,valor,quantidade\n');
         writeCSV (filePath, dados);
     }
+    async valorTotal(data:Data) {
+        function multiplicar(valor:number, quantidade:number) {
+            return valor * quantidade;
+        }
+
+        const valor = data.valor;
+        const quantidade = data.quantidade;
+        const dados = await readCSV(filePath);
+        if(dados.length == 0){
+            throw new Error ('Estoque vazio')
+        }
+        return multiplicar;
+    }
 }
 
 export default new ServiceEstoque;
